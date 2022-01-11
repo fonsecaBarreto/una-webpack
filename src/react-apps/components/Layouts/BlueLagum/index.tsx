cbda5c0893ec7cf5873b13b93442eb93b6ecfe3f
fonsecaBarretom <lucasbfonte@gmail.com>
2022-01-11 19:45:34 -0300

    Setado um botão padrão para una, um 'Dialog' de alerta ao tentar entrar no carrinho sem ter um usuario

--- src/react-apps/components/Layouts/BlueLagum/index.tsx
+++ src/react-apps/components/Layouts/BlueLagum/index.tsx
@@ -1,22 +1,46 @@
 
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './style.css'
 
 import LayoutHeader from './Header'
 import LayoutFooter from './Footer' 
 import LayoutCart from './Cart'
 import { useDispatch, useSelector } from 'react-redux'
-import { setUser } from '@/react-apps/store/reducers/main/actions'
+import globalComponent from '@/react-apps/apps/main/global/global-components-context';
+import { MakeDialogConfig } from 'fck-react-dialog'
+import ForbiddenCartModal from '../../Modals/ForbiddenCart'
 
+import { IoMdLogIn } from 'react-icons/io'
+import { useHistory } from 'react-router-dom'
 
 const PrimaryLayout:  React.FunctionComponent<any> = ({children}) =>{
+    const history = useHistory()
+    const Context: any = useContext(globalComponent);
     const { user } = useSelector((state: any)=>state.main)
     const [ showCart, setShowCart ] = useState(false)
+
+    const openCart = () =>{
+
+        if(!user){
+
+            return Context.dialog.push( MakeDialogConfig(ForbiddenCartModal,
+                (n)=>{
+                    switch(n){
+                        case 1: history.push("/login?v=singin");break;
+                        case 2: history.push("/login?v=signup");break;
+                    }
+                    return -1;
+                }, "UNA-SE AO JOGO DOS GRANDES"))
+
+        }else{
+            setShowCart(!showCart) 
+        }
+    }
 
     return (
         <div className="blue-lagum">
             <header>
-                <LayoutHeader toggleCart={()=>setShowCart(!showCart)} user={user}></LayoutHeader>
+                <LayoutHeader toggleCart={openCart} user={user}></LayoutHeader>
             </header> 
              <main>
                 {children}
