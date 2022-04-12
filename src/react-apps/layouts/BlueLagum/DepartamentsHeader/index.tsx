2cf81a4d16dafcab64144ad388ce2db300852433
fonsecaBarreto <lucasbfonte@gmail.com>
2022-04-12 11:21:21 -0300

    integration

--- src/react-apps/layouts/BlueLagum/DepartamentsHeader/index.tsx
+++ src/react-apps/layouts/BlueLagum/DepartamentsHeader/index.tsx
@@ -1,14 +1,17 @@
 import React, { FunctionComponent } from 'react'
+import { GiFruitBowl } from 'react-icons/gi'
 import { IoMdArrowDropdown } from 'react-icons/io'
+import { MdBakeryDining } from 'react-icons/md'
+import { RiShoppingBasketLine } from 'react-icons/ri'
 import { Link } from 'react-router-dom'
 import './style.css'
 
 export const DepartamentDropDown: FunctionComponent<any> = ({children}) => {
     return (
         <div className='departament-dropdown'>
-            <div>
-                <IoMdArrowDropdown/> {children}
-            </div>
+            
+            {children}
+         
         </div>
     )
 }
@@ -19,16 +22,23 @@ export const DepartamentHeader = () => {
             <div className='app-container'>
                 <nav>
                     <DepartamentDropDown>
-                        <Link className='dep-link' to="/mercado">Todos departamentos</Link>
+                        <Link className='dep-link' to="/mercado">
+                            <IoMdArrowDropdown/>
+                            Todos departamentos</Link>
                     </DepartamentDropDown> 
                     <DepartamentDropDown>
-                        <Link className='dep-link' to="/mercado/padaria"> Padaria </Link>
+                        <Link className='dep-link' to="/mercado/padaria"> 
+                            <MdBakeryDining></MdBakeryDining>
+                            Padaria
+                         </Link>
                     </DepartamentDropDown> 
                     <DepartamentDropDown>
-                        <Link className='dep-link' to="/mercado/hortifruti"> Hortifruti </Link>
+                        <Link className='dep-link' to="/mercado/hortifruti">
+                            <GiFruitBowl/>
+                             Hortifruti </Link>
                     </DepartamentDropDown> 
                     <DepartamentDropDown>
-                        <Link className='dep-link' to="/mercado/mercearia"> Mercearia </Link>
+                        <Link className='dep-link' to="/mercado/mercearia"> <RiShoppingBasketLine/>Mercearia </Link>
                     </DepartamentDropDown> 
                 </nav>
             </div>
