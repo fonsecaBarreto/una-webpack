966eabe19efb9598c627a99069fb3a79372710bd
fonsecaBarreto <lucasbfonte@gmail.com>
2022-10-17 10:34:14 -0300

    feat: global keys

--- src/services/global-keys.ts
+++ src/services/global-keys.ts
@@ -1,7 +1,7 @@
 
 export const global = {
-  user_storage_key: "una-compras",
-  location_storage_key: "una-compas-localidade",
+  user_storage_key: "@unacompras:token",
+  location_storage_key: "@unacompras:localidade",
   base_url: process.env.API_URL,
   google_api_key: process.env.GOOGLE_API_KEY
 }
