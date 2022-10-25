e18f639bce59ca43556988ea3216ee50d4563992
fonsecaBarreto <lucasbfonte@gmail.com>
2022-10-25 17:49:04 -0300

    fix: product page image

--- src/react-apps/pages/product-page/GaleryContainer/components/PhotoFrame.tsx
+++ src/react-apps/pages/product-page/GaleryContainer/components/PhotoFrame.tsx
@@ -3,18 +3,18 @@ import ImageNotFount from "@/public/assets/images/product/empty.svg"
 import { filesService } from '@/services/api/files-service'
 
 export const PhotoFrame: React.FunctionComponent<any> = ({ img }) => {
-    const [image, setimage ] = React.useState<{ url: string }>({ url: ImageNotFount });
+   /*  const [image, setimage ] = React.useState<{ url: string }>({ url: ImageNotFount });
 
     React.useEffect(()=>{
         setimage((prev:any)=>{
             if(!img) return { url: ImageNotFount};
             return { ...img, url:  filesService.get_public_images_url( img + "/" + 560 + ".jpeg") }
         })
-    },[img])
+    },[img]) */
 
     return (
         <section className="galery-container-photo-frame">
-             <img src={image?.url}></img> 
+             <img src={img}></img> 
         </section>
     )
 } 
