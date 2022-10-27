d8cd3e2bb6cb7a2532259a4ddb9c2c8d98082495
fonsecaBarreto <lucasbfonte@gmail.com>
2022-10-27 14:32:40 -0300

    remove deprecated menu options

--- src/react-apps/layouts/BlueLagum/Menu/TREE.tsx
+++ src/react-apps/layouts/BlueLagum/Menu/TREE.tsx
@@ -1,16 +1,14 @@
 
 import React from 'react'
-import BusinnesIcon from "@assets/icons/business.svg"
-import AddIcon from "@assets/icons/add-register.svg"
-
 import DocumentsFolder from "@assets/icons/documents-folder.svg"
 import WhatsAppIcon from "@assets/icons/whatsapp.svg"
-import ProfileIcon from "@assets/icons/profile.svg"
 import { UserProfileRole } from '@/domain/views/User'
 import HomeIcon from "@assets/icons/home.svg"
 import ExitIcon from "@assets/icons/exit.svg"
 import DocumentsIcons from "@assets/icons/cotacoes.svg"
 import { loginServices } from '@/services/api/login-service'
+import AdminIcon from "@assets/icons/admin.png"
+
 const svgStyle = {
     width: "18px",
     filter: "invert(5%) sepia(80%) saturate(5600%) hue-rotate(240deg) brightness(90%) contrast(150%)"
@@ -26,22 +24,11 @@ export const ADMIN_TREE_MENU = (user: any) =>{
            toDo:"/admin/cotacoes",
            icon: <img src={DocumentsFolder} style={svgStyle}/>
        },
-
        { 
-           label:"Companhias", 
-           toDo:"/admin/companhias", 
-           icon: <img src={BusinnesIcon} style={svgStyle}/>
-       },
-
-       { 
-           label:"Registro", 
-           toDo:"/admin/registro", 
-           icon: <img src={AddIcon} style={svgStyle}/>
-       }, 
-     /*   { 
-           label:"Perfil",  toDo:`/perfil/companhias/${user.company_id}`,
-           icon: <img src={ProfileIcon}style={svgStyle}></img>
-       }, */
+           label:"Admin", 
+           toDo: () => window.location.href="https://app.unacompras.com.br/admin",
+           icon: <img src={AdminIcon} style={svgStyle}/>
+       }
    ]
 }
 
