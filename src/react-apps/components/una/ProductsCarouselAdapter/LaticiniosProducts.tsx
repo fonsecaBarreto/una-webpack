915bdd428977cd5c417c4f346f4253be3e514786
fonsecaBarreto <lucasbfonte@gmail.com>
2022-11-25 10:56:19 -0300

    feat: add laticinios carousel

---
+++ src/react-apps/components/una/ProductsCarouselAdapter/LaticiniosProducts.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import './style.css'
+import Item from "./item"
+import { produtosServiceV2 } from "@/services/api/v2/produtos-service"
+import UtilsCarousel from '../../utils/Carousel';
+import { useHistory } from 'react-router-dom';
+
+export const LaticiniosProductsCarousel: React.FunctionComponent<any> = () =>{
+    const [products, setProducts] = React.useState<any>([])
+    const history = useHistory();
+    React.useEffect(()=>{
+        produtosServiceV2.list({categories: ["queijos"]})
+            .then(r=>{setProducts(r.records)});
+    },[])
+
+
+    const handleChanges = (k: string, p?: any) =>{
+        switch(k){
+            case "OPEN":
+                history.push(`/produto/${p}`)
+            break;
+        }
+    }
+
+    return (
+        <div className='common-products-carousel'>
+            <UtilsCarousel 
+                onChange={handleChanges}
+                element={Item} 
+                records={products} 
+                colums={[4,4,3,3,2]}/>
+        </div>
+                 
+    )
+}
+
+export default LaticiniosProductsCarousel
--- src/react-apps/pages/HomePage/index.tsx
+++ src/react-apps/pages/HomePage/index.tsx
@@ -8,6 +8,7 @@ import HeadLineCarousel from '@/react-apps/components/una/ProductsCarouselAdapte
 import LatestProductsCarousel from '@/react-apps/components/una/ProductsCarouselAdapter/LatestProducts';
 import { useSelector } from 'react-redux';
 import ConfeitareProductsCarousel from '@/react-apps/components/una/ProductsCarouselAdapter/ConfeitareProducts';
+import LaticiniosProductsCarousel from '@/react-apps/components/una/ProductsCarouselAdapter/LaticiniosProducts';
 
 export const HomePage: React.FunctionComponent<any> = ({ history }) => {
 
@@ -21,18 +22,23 @@ export const HomePage: React.FunctionComponent<any> = ({ history }) => {
                 <CategoriasCarousel/>
 
                 <section className='una-home-section'>
+                    <h4> Laticinios: </h4>
+                    <LaticiniosProductsCarousel/>
+                </section> 
+                
+                <section className='una-home-section'>
                     <h4> Para sua padaria: </h4>
                     <PadariaProductsCarousel/>
                 </section> 
  
                 <section className='una-home-section'>
-                    <h4> Produtos de limpeza: </h4>
-                    <LimpezaProductsCarousel/>
+                    <h4> Doces: </h4>
+                    <ConfeitareProductsCarousel/>
                 </section> 
 
                 <section className='una-home-section'>
-                    <h4> Doces: </h4>
-                    <ConfeitareProductsCarousel/>
+                    <h4> Produtos de limpeza: </h4>
+                    <LimpezaProductsCarousel/>
                 </section> 
 
                 <LatestProductsCarousel/>
