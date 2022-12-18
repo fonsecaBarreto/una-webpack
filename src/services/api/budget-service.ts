a862bd78b67e6a25f9d266d37b0ac941e15a9894
fonsecaBarreto <lucasbfonte@gmail.com>
2022-12-18 20:10:54 -0300

    refactor: new budgets services

--- src/services/api/budget-service.ts
+++ src/services/api/budget-service.ts
@@ -1,33 +1,49 @@
-import { Product } from '@/domain/views/Product'
 import { global } from '@/services/global-keys'
 import { MakeApiSettings, errorHandler } from './helpers'
 
 const budgetsApi = MakeApiSettings({
-     base_url: `${global.base_url}/budgets`,
+     base_url: `${global.base_api}/v2/budgets`,
      errorHelper: errorHandler, 
      storage_key: global.user_storage_key
 })
 
 export namespace BudgetServices {
+     export type Item = {
+          ean: string, 
+          quantity: string, 
+          supply: { supplier_id: string, index: number }
+     }
+     
      export type SaveParams = {
-        company_id: string,
-        products: { ean: string, quantity: string}[]
-    }
-   
+        items: { ean: string, quantity: string , supply: { supplier_id: string, index: number }}[]
+     }
+}
+
+
+export namespace ListBudgetServices {
+     
+     export type Params = {}
 }
 
 export const budgetServices = {
 
      save: async (params: BudgetServices.SaveParams) => {
-          const { company_id, ...rest } = params;
-          const data = { ...rest };
-          const METHOD =  "POST"
-          const URL = `/company/${company_id}`
-          const resp = await budgetsApi.send({ method: METHOD, url: URL, data }) 
+          const data = { ...params };
+          const resp = await budgetsApi.send({ method: "POST", url: "/", data }) 
           return resp.data 
      },
-     /* ADMIN listing budgets */
+
      list: async (params: any={}) => {
+          const { p = 1, initial_date, end_date } = params;
+          var query = `?page=${p}&limit=99999`;
+        /*   query+=`
+               &idate=${new Date(initial_date).getTime()} 
+               &ldate=${new Date(end_date).getTime()}`; */
+          const { data } = await budgetsApi.send({ method: "get", url: `/${query}` }) 
+          return data 
+     },
+     /* ADMIN listing budgets */
+  /*    list: async (params: any={}) => {
           const { p = 1, initial_date, end_date, company_id } = params;
           var query = `?page=${p}`;
           query+=`
@@ -37,36 +53,28 @@ export const budgetServices = {
 
           const { data } = await budgetsApi.send({ method: "get", url: `${query}` }) 
           return data 
-     },
-     /* admins finding budgets */
+     }, */
+    /*  // admins finding budgets 
      find: async (budget_id: string) => {
           const { data } = await budgetsApi.send({ method: "get", url: `/${budget_id}` }) 
           return data 
      },
-     /* admins update */
+     // admins update 
 
      updateStatus: async (budget_id: string, status: string) => {
           await budgetsApi.send({ method: "patch", url: `/${budget_id}/status`, data: { status } }) 
           return;
      },
-
+ */
 
      /*  Clientes */
 
-     findCompanyBudget: async (budget_id: string, company_id:string) => {
+ /*     findCompanyBudget: async (budget_id: string, company_id:string) => {
           const { data } = await budgetsApi.send({ method: "get", url: `/company/${company_id}/${budget_id}` }) 
           return data 
      },
      
-     listCompanyBudgets: async (params: any={}) => {
-          const { p = 1, initial_date, end_date, company_id } = params;
-          var query = `?page=${p}`;
-          query+=`
-               &idate=${new Date(initial_date).getTime()} 
-               &ldate=${new Date(end_date).getTime()}`;
-          const { data } = await budgetsApi.send({ method: "get", url: `/company/${company_id}${query}` }) 
-          return data 
-     },
-
+    
+ */
 }
 
