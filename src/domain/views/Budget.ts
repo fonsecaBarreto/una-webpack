eab45624b1cb6b6414d3f4666be4edae596e0899
fonsecaBarreto <lucasbfonte@gmail.com>
2022-12-18 20:12:22 -0300

    fix: budget model

--- src/domain/views/Budget.ts
+++ src/domain/views/Budget.ts
@@ -8,19 +8,25 @@ export enum BudgetStatus{
     CLOSED="CLOSED"             
 }
 export interface Budget {
-    id: number,
-    user_id:string
-    amount: number,
-    created_at: Date,
-    updated_at: Date
+    id: number;
+    user_id: string;
+    company_id: string;
+    amount: number;
+    status: BudgetStatus;
+    created_at: Date;
+    updated_at: Date;
 }
 
 // FK [ budget_id, product_id ]
 export interface BudgetItem {
-    budget_id: number
-    product_id: string,
-    quantity: number,
-    price: number
+    budget_id: number;
+    ean: string;
+    quantity: number;
+    price: number;
+    supply_supplier_id: string | null;
+    supply_index: number | null;
+    order: number;
+    // primary = budget_id + order
 }
 
 
