6c0e35608cb03962bc6abc43a191decfb7635e21
fonsecaBarreto <lucasbfonte@gmail.com>
2022-12-18 20:13:12 -0300

    implement: create budget service

--- src/react-apps/layouts/BlueLagum/Cart/CartGroup/index.tsx
+++ src/react-apps/layouts/BlueLagum/Cart/CartGroup/index.tsx
@@ -1,17 +1,22 @@
-import { CartState, setCart } from '@/react-apps/store/reducers/cart'
+import { CartState, removeFromCart, setCart } from '@/react-apps/store/reducers/cart'
 import React, { useContext, useEffect, useMemo, useState } from 'react'
 import CartItem from "../CartItem"
 import "./styles.css"
 
 import { useSelector, useDispatch} from 'react-redux'
 import { setLoading } from '@/react-apps/store/reducers/main/actions'
-import { budgetServices } from '@/services/api/budget-service'
+import { BudgetServices, budgetServices } from '@/services/api/budget-service'
+import { MakeNotification, NotificationType } from 'fck-react-dialog'
+import GlobalContext from '@/react-apps/apps/GlobalContext'
+import { useHistory } from 'react-router-dom'
 
-export const CartGroup: React.FunctionComponent<any> = ({ items, label, supplier_id }) =>{
+export const CartGroup: React.FunctionComponent<any> = ({ items, label, supplier_id, onChange}) =>{
 
+    const context = useContext(GlobalContext)
+    const history = useHistory()
     const { user } = useSelector((state: any)=>state.main);
-
     const dispatch = useDispatch();
+
     const isReady = useMemo(() =>{
         if(items?.length == 0) return false;
         let isReady = true;
@@ -24,7 +29,6 @@ export const CartGroup: React.FunctionComponent<any> = ({ items, label, supplier
         return isReady;
     },[items])
 
-
     const getSomatorio = () =>{
         let sum = 0;
         items?.length > 0 && items.map((item_data: any)=> {
@@ -36,20 +40,26 @@ export const CartGroup: React.FunctionComponent<any> = ({ items, label, supplier
 
 
     const submit = async () =>{
+
+        const _ids: string[] = [];
+        const serialized_cart_items:BudgetServices.Item[] = items.map((item:any)=>{
+            const { _id, qtd } = item
+            const [ ean, index, supplier_id ] = _id.split("_")
+            _ids.push(_id)
+            return { 
+                ean, quantity: qtd,
+                supply: index != "undefined" && supplier_id != "undefined" ? { index, supplier_id } : null
+            }
+        });
+
         try{
             dispatch(setLoading(true))
-            await budgetServices.save({
-                products: items.map((c:any)=>({ ean: c.product.ean, quantity: c.qtd})),
-                company_id: user.company_id
-            });
-
-            alert('Orçamento feito com sucesso!')
-            // context.dialog.push(MakeNotification(()=>{return -1},["Obrigado.", "Estamos negociando com fornedores para garantir o melhor preço!","aguarde o nosso contato em até 24 horas."], "Sucesso!", NotificationType.SUCCESS))
+            await budgetServices.save({ items: serialized_cart_items });
+            dispatch(removeFromCart(_ids))
+            context.dialog.push(MakeNotification(()=>{return -1},["Obrigado.", "Estamos negociando com fornedores para garantir o melhor preço!","aguarde o nosso contato em até 24 horas."], "Sucesso!", NotificationType.SUCCESS))
             
-            dispatch(setCart([]))
         }catch(err: any){
-            console.log(err)
-            // context.dialog.push(MakeNotification(()=>{return -1},[err.mesage ?? "Erro inesperado"], "Erro!", NotificationType.SUCCESS))
+            context.dialog.push(MakeNotification(()=>{return -1},[err.mesage ?? "Não foi possível concluir essa cotação! Por favor, entre em contato com o suporte."], "Desculpe!", NotificationType.SUCCESS))
         }
         finally{
             dispatch(setLoading(false))
