9ba835923316ea73049d05dcde7c4631242377b8
fonsecaBarreto <lucasbfonte@gmail.com>
2022-12-18 22:55:42 -0300

    feat: add new headline

--- src/react-apps/components/una/ProductsCarouselAdapter/HeadLineCarousel.tsx
+++ src/react-apps/components/una/ProductsCarouselAdapter/HeadLineCarousel.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { Carousel } from 'react-bootstrap';
-import TestImage from "@assets/headline/headline1-lg.webp"
-import TestImageMd from "@assets/headline/headline1-md.webp"
+/* import TestImage from "@assets/headline/headline1-lg.webp" */
+import TestImage2 from "@assets/headline/headline2-lg.webp"
+import TestImageMd2 from "@assets/headline/headline2-md.webp"
+/* import TestImageMd from "@assets/headline/headline1-md.webp" */
 
 export const HeadLineCarousel: React.FunctionComponent<any> = () =>{
     const [index, setIndex] = React.useState(0);
@@ -14,13 +16,22 @@ export const HeadLineCarousel: React.FunctionComponent<any> = () =>{
         <>
             <Carousel activeIndex={index} onSelect={handleSelect}>
 
+             
+
                 <Carousel.Item>
+                    <picture >
+                        <source srcSet={TestImage2} media="(min-width: 756px)"/>
+                        <img className="d-block w-100" src={TestImageMd2}  alt="First slide"/>
+                    </picture>
+                </Carousel.Item>
+
+               {/*  <Carousel.Item>
                     <picture >
                         <source srcSet={TestImage} media="(min-width: 756px)"/>
                         <img className="d-block w-100" src={TestImageMd}  alt="First slide"/>
                     </picture>
                 </Carousel.Item>
-
+ */}
             </Carousel>
             
         </>
