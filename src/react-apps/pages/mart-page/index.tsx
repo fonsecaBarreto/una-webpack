04f8b84475bd25bc1c4881fefe209aab4f03c7ea
fonsecaBarreto <lucasbfonte@gmail.com>
2022-12-28 15:45:44 -0300

    fix: scroll behavior on mart page

--- src/react-apps/pages/mart-page/index.tsx
+++ src/react-apps/pages/mart-page/index.tsx
@@ -5,10 +5,15 @@ import ProductFeed from '@/react-apps/pages/mart-page/ProductFeed'
 import UseFetchManager from './FetchManager'
 import CategoriasNav from './CategoriasNav'
 import LatestProductsCarousel from '@/react-apps/components/una/ProductsCarouselAdapter/LatestProducts';
+import GlobalContext from '@/react-apps/apps/GlobalContext';
 
 export const MartPage: React.FunctionComponent<any> = ({ history }) => {
-
+    const context = React.useContext(GlobalContext);
     const fetchManager = UseFetchManager();
+
+    React.useEffect(()=>{
+        context.app.current?.scrollTo({ top: 0, behavior: 'auto'}); 
+    },[fetchManager.values])
 
     return (
         <div id="departamento-page">
@@ -18,7 +23,6 @@ export const MartPage: React.FunctionComponent<any> = ({ history }) => {
                    <CategoriasNav manager={fetchManager}></CategoriasNav> 
                     <ProductFeed manager={fetchManager} ></ProductFeed>
                 </ContentGrid> 
-
                 <LatestProductsCarousel/>
                 
             </div> 
