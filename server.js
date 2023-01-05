a0b204be0d8df48becf29d0cd975066418fc0a99
fonsecaBarreto <lucasbfonte@gmail.com>
2023-01-05 15:07:45 -0300

    fix: server path config

--- server.js
+++ server.js
@@ -8,23 +8,11 @@ const PORT = process.env.PORT || 8080;
 
 app.use("/", express.static(path.resolve(__dirname, "dist")));
 
-/* app.use(function(req, res, next){
-  try{
-    decodeURIComponent(req.path)
-  }catch(e){
-    console.log(e, req.url);
-    return res.redirect(["https://", req.get("Host"), "/404"].join(""));
-  }
-  next();
-}) */
 
-/* app.get(`/`, (req, res) => {
+app.get(`/bem-vindo/:path*?`, (req, res) => {
   return res.sendFile(path.resolve(__dirname, "dist", "views", `landingpage.html`));
-});  */
-
-app.get(`/admin/:path*?`, (req, res) => {
-  return res.sendFile(path.resolve(__dirname, "dist", "views", `admin.html`));
 }); 
+
 
 app.get(`/:path*?`, (req, res) => {
     return res.sendFile(path.resolve(__dirname, "dist", "views", `main.html`));
@@ -34,3 +22,17 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
 });
 
+
+/* app.use(function(req, res, next){
+  try{
+    decodeURIComponent(req.path)
+  }catch(e){
+    console.log(e, req.url);
+    return res.redirect(["https://", req.get("Host"), "/404"].join(""));
+  }
+  next();
+}) */
+/* app.get(`/admin/:path*?`, (req, res) => {
+  return res.sendFile(path.resolve(__dirname, "dist", "views", `admin.html`));
+}); 
+ */
