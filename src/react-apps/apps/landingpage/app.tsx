03e51b2245c6e2da46e8cd556f87a578fa4276ad
fonsecaBarreto <lucasbfonte@gmail.com>
2023-01-05 15:08:18 -0300

    feat: init new bundle landingpage

--- src/react-apps/apps/landingpage/app.tsx
+++ src/react-apps/apps/landingpage/app.tsx
@@ -1,12 +1,11 @@
 import React, { useRef } from "react";
 import '../app.css'
-import Routes from './routes' 
 
 export const App = () =>{
     const appRef = useRef<HTMLHeadingElement>(null)
     return (
         <div id="App" ref={appRef} > 
-            <Routes></Routes> 
+            Testes
         </div>
     )
 }
