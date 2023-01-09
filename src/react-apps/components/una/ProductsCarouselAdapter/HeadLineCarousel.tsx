151e1473161cef2e9ed44522b67e1709fc4d995d
fonsecaBarreto <lucasbfonte@gmail.com>
2023-01-09 18:32:10 -0300

    feat: preloading image on headlione

--- src/react-apps/components/una/ProductsCarouselAdapter/HeadLineCarousel.tsx
+++ src/react-apps/components/una/ProductsCarouselAdapter/HeadLineCarousel.tsx
@@ -1,31 +1,39 @@
-import * as React from 'react';
-import { Carousel } from 'react-bootstrap';
-import TestImage2 from "@assets/headline/dist/headline-lg.webp"
-import TestImageMd2 from "@assets/headline/headline2-md.webp"
+import * as React from "react";
+import { Carousel } from "react-bootstrap";
+import TestImage2 from "@assets/headline/dist/headline-lg.webp";
+import TestImageMd2 from "@assets/headline/headline2-md.webp";
 
-export const HeadLineCarousel: React.FunctionComponent<any> = () =>{
-    const [index, setIndex] = React.useState(0);
+export const HeadLineCarousel: React.FunctionComponent<any> = () => {
+  const [index, setIndex] = React.useState(0);
 
-    const handleSelect = (selectedIndex: number, e: any) => {
-      setIndex(selectedIndex);
-    };
-    
-    return (
-        <>
-            <Carousel activeIndex={index} onSelect={handleSelect}>
+  React.useEffect(() => {
+    //preloading image
+    [TestImage2, TestImageMd2].forEach((img_src) => {
+      const img = new Image();
+      img.src = img_src;
+    });
+  }, []);
 
-                <Carousel.Item>
-                    <picture >
-                        <source srcSet={TestImage2} media="(min-width: 756px)"/> 
-                        <img className="d-block w-100" src={TestImageMd2}  alt="First slide"/>
-                    </picture>
-                </Carousel.Item>
+  const handleSelect = (selectedIndex: number, e: any) => {
+    setIndex(selectedIndex);
+  };
 
-            </Carousel>
-            
-        </>
-  
-    )
-}
+  return (
+    <>
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <picture>
+            <source srcSet={TestImage2} media="(min-width: 756px)" />
+            <img
+              className="d-block w-100"
+              src={TestImageMd2}
+              alt="First slide"
+            />
+          </picture>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+};
 
-export default HeadLineCarousel
+export default HeadLineCarousel;
