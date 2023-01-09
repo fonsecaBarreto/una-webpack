ad7db55024495f636ada8580e58dd25eff5f604d
fonsecaBarreto <lucasbfonte@gmail.com>
2023-01-09 17:49:17 -0300

    feat: change headline image

--- src/react-apps/components/una/ProductsCarouselAdapter/HeadLineCarousel.tsx
+++ src/react-apps/components/una/ProductsCarouselAdapter/HeadLineCarousel.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import { Carousel } from 'react-bootstrap';
-/* import TestImage from "@assets/headline/headline1-lg.webp" */
-import TestImage2 from "@assets/headline/headline2-lg.webp"
+import TestImage2 from "@assets/headline/dist/headline-lg.webp"
 import TestImageMd2 from "@assets/headline/headline2-md.webp"
-/* import TestImageMd from "@assets/headline/headline1-md.webp" */
 
 export const HeadLineCarousel: React.FunctionComponent<any> = () =>{
     const [index, setIndex] = React.useState(0);
@@ -16,22 +14,13 @@ export const HeadLineCarousel: React.FunctionComponent<any> = () =>{
         <>
             <Carousel activeIndex={index} onSelect={handleSelect}>
 
-             
-
                 <Carousel.Item>
                     <picture >
-                        <source srcSet={TestImage2} media="(min-width: 756px)"/>
+                        <source srcSet={TestImage2} media="(min-width: 756px)"/> 
                         <img className="d-block w-100" src={TestImageMd2}  alt="First slide"/>
                     </picture>
                 </Carousel.Item>
 
-               {/*  <Carousel.Item>
-                    <picture >
-                        <source srcSet={TestImage} media="(min-width: 756px)"/>
-                        <img className="d-block w-100" src={TestImageMd}  alt="First slide"/>
-                    </picture>
-                </Carousel.Item>
- */}
             </Carousel>
             
         </>
