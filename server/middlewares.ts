e8cea30b6d771530c866ce0f58b33fa99d059a4f
fonsecaBarreto <lucasbfonte@gmail.com>
2023-01-09 23:51:41 -0300

    feat: add compression middleware

--- server/middlewares.ts
+++ server/middlewares.ts
@@ -1,5 +1,10 @@
 import express, { Express } from 'express'
 import cors from "cors"
+import compression from "compression"
+
+
 export function useMiddlewares(app: Express){
     app.use(cors());
+    app.use(compression());
+
 }
