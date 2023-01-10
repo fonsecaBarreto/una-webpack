e07e117df88a7433c4915c2a8ddeea86508b4e1d
fonsecaBarreto <lucasbfonte@gmail.com>
2023-01-09 22:26:20 -0300

    test google ads

--- src/react-apps/pages/mart-page/index.tsx
+++ src/react-apps/pages/mart-page/index.tsx
@@ -20,7 +20,7 @@ export const MartPage: React.FunctionComponent<any> = ({ history }) => {
             <div className='app-container'>
                 
                 <ContentGrid loading={fetchManager.isLoading}>
-                   <CategoriasNav manager={fetchManager}></CategoriasNav> 
+                    <CategoriasNav manager={fetchManager}></CategoriasNav> 
                     <ProductFeed manager={fetchManager} ></ProductFeed>
                 </ContentGrid> 
                 <LatestProductsCarousel/>
